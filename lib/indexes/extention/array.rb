942331323111c50122dc5e2df21abed060b24305
onigra <3280467rec@gmail.com>
2014-07-31 18:28:41 +0900

    fixed

--- lib/indexes/extention/array.rb
+++ lib/indexes/extention/array.rb
@@ -1,5 +1,33 @@
 class Array
+
   def indexes(item)
     self.each_with_index.map { |obj, index| index if obj == item }.reject(&:nil?)
+  end
+
+  def slice_indexes(item)
+    result = []
+    tmp = self.indexes item
+
+    tmp.each_with_index do |obj, index|
+      # indexが最後だったら
+      if index == (tmp.size - 1)
+
+        # objの位置から末尾までを取得
+        result << self[obj..(self.size - 1)]
+      else
+
+        # [1, 1]みたいに取得したい値(item)の値が続いている場合
+        # 取得したindexが連番になっているはず
+        if tmp[index + 1] - obj == 1
+          result << [self[obj]]
+        else
+
+          # objの位置から次のindex - 1までを取得
+          result << self[obj..(tmp[index + 1] - 1)]
+        end
+      end
+    end
+
+    result
   end
 end
--- spec/extention/array_spec.rb
+++ spec/extention/array_spec.rb
@@ -15,5 +15,35 @@ describe Array do
 
       it { should eq [0, 3, 5, 6, 9] }
     end
+
+    context 'case3' do
+      let(:ary) { ["foo", "bar", "baz", "foo", "baz", "foo"] }
+      subject { ary.indexes "foo" }
+
+      it { should eq [0, 3, 5] }
+    end
+  end
+
+  describe '#slice_indexes' do
+    context 'case1' do
+      let(:ary) { [1, 2, 3, 1, 2] }
+      subject { ary.slice_indexes 1 }
+
+      it { should eq [[1, 2, 3], [1, 2]] }
+    end
+
+    context 'case2' do
+      let(:ary) { [1, 2, 3, 1, 2, 1, 1, 3, 2, 1] }
+      subject { ary.slice_indexes 1 }
+
+      it { should eq [[1, 2, 3], [1, 2], [1], [1, 3, 2], [1]] }
+    end
+
+    context 'case3' do
+      let(:ary) { ["foo", "bar", "baz", "foo", "baz", "foo"] }
+      subject { ary.slice_indexes "foo" }
+
+      it { should eq [["foo", "bar", "baz"], ["foo", "baz"], ["foo"]] }
+    end
   end
 end
