311459cd847f2d4071a79b4cdf9bdf8a188b667c
onigra <3280467rec@gmail.com>
2015-01-19 16:05:08 +0900

    変数名の修正 itemをmatch_condition(一致条件)にした

--- lib/slice_by_indexes/extention/array.rb
+++ lib/slice_by_indexes/extention/array.rb
@@ -1,21 +1,21 @@
 class Array
 
-  def indexes(item = nil)
+  def indexes(match_condition = nil)
     if block_given?
       self.each_with_index.map { |obj, index| index if yield(obj) }.reject(&:nil?)
     else
-      self.each_with_index.map { |obj, index| index if obj == item }.reject(&:nil?)
+      self.each_with_index.map { |obj, index| index if obj == match_condition }.reject(&:nil?)
     end
   end
 
-  def slice_by_indexes(item = nil)
+  def slice_by_indexes(match_condition = nil)
     result = []
     tmp = nil
 
     if block_given?
       tmp = self.indexes { |i| yield(i) }
     else
-      tmp = self.indexes item
+      tmp = self.indexes match_condition
     end
 
     tmp.each_with_index do |obj, index|
@@ -26,7 +26,7 @@ class Array
         result << self[obj..(self.size - 1)]
       else
 
-        # [1, 1]みたいに取得したい値(item)の値が続いている場合
+        # [1, 1]みたいに取得したい値(match_condition)の値が続いている場合
         # 取得したindexが連番になっているはず
         if tmp[index + 1] - obj == 1
           result << [self[obj]]
